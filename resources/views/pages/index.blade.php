6c90cef71425fa6d056504e6ac264e8f00c111f1
developermithu <developermithu@gmail.com>
2024-07-14 07:41:14 +0600

    install livewire and volt

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+<?php
+
+use Livewire\Volt\Component;
+use Livewire\Attributes\{Layout, Title};
+
+new 
+#[Layout('components.layouts.app')]
+class extends Component {
+    //
+}; ?>
+
+<div>
+    index home
+</div>
