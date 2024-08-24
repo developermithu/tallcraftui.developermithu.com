75ed9c71ffb46312e501d3840bf10745eba387c5
developermithu <developermithu@gmail.com>
2024-08-24 16:29:24 +0600

    added favicon icons

--- resources/views/components/meta-tags.blade.php
+++ resources/views/components/meta-tags.blade.php
@@ -11,11 +11,20 @@
 <meta property="og:url" content="{{ url()->current() }}">
 <meta property="og:title" content="{{ $title }}">
 <meta property="og:description" content="{{ $description ?? 'TallCraftUI offers beautifully crafted UI components for the TALL stack (Tailwind CSS, Alpine.js, Laravel, Livewire).' }}">
-<meta property="og:image" content="{{ $image ?? asset('tallcraftui.png') }}">
+<meta property="og:image" content="{{ $image ?? asset('assets/img/tallcraftui-website.png') }}">
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image">
 <meta name="twitter:creator" content="@DeveloperMithu">
 <meta name="twitter:title" content="{{ $title }}">
 <meta name="twitter:description" content="{{ $description ?? 'TallCraftUI offers beautifully crafted UI components for the TALL stack (Tailwind CSS, Alpine.js, Laravel, Livewire).' }}">
-<meta name="twitter:image" content="{{ $image ?? asset('tallcraftui.png') }}">
+<meta name="twitter:image" content="{{ $image ?? asset('assets/img/tallcraftui-website.png') }}">
+
+
+<link rel="canonical" href="https://tallcraftui.developermithu.com">
+
+{{-- Favicon --}}
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('assets/img/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('assets/img/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('assets/img/favicon-16x16.png') }}">
+<link rel="manifest" href="/site.webmanifest">
