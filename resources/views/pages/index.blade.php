38f795b313397ad01f81fb54c6bc245b9e909a4e
developermithu <developermithu@gmail.com>
2024-10-15 21:16:27 +0600

    added avatar components example in homepage

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -111,6 +111,36 @@ new #[Layout('components.layouts.home')] #[Title('TallCraftUI - Laravel Blade UI
                         </x-card-content>
                     </x-card>
 
+                    {{-- Avatar Components --}}
+                    <x-card>
+                        <x-card-header title="Avatar Components" class="flex items-center gap-3" >
+                            <x-badge label="New" amber />
+                        </x-card-header>
+                        <x-card-content>
+                            <div class="space-y-3">
+                                <div class="flex flex-wrap items-start gap-x-3 gap-y-2">
+                                    <x-avatar lg />
+                                    <x-avatar text="tallcraftui" lg />
+                                    <x-avatar image="{{ asset('assets/img/avatar.jpg') }}" lg />
+                                    <x-avatar image="{{ asset('assets/img/avatar.jpg') }}" badge lg />
+                                    <x-avatar image="{{ asset('assets/img/avatar.jpg') }}" ring lg />
+                                    <x-avatar image="{{ asset('assets/img/avatar.jpg') }}" ring badge lg />
+                                    <x-avatar image="{{ asset('assets/img/avatar.jpg') }}" rounded ring badge badge-position="bottom" lg />
+                                </div>
+                                
+                                <div class="flex flex-wrap items-start gap-x-3 gap-y-2">
+                                    <x-avatars stacked plus="99" ring lg>
+                                        <x-avatar image="{{ asset('assets/img/avatar.jpg') }}" ring lg />
+                                        <x-avatar image="{{ asset('assets/img/avatar.jpg') }}" ring lg />
+                                        <x-avatar image="{{ asset('assets/img/avatar.jpg') }}" ring lg />
+                                        <x-avatar image="{{ asset('assets/img/avatar.jpg') }}" ring lg />
+                                        <x-avatar image="{{ asset('assets/img/avatar.jpg') }}" ring lg />
+                                    </x-avatars>
+                                </div>
+                            </div>
+                        </x-card-content>
+                    </x-card>
+
                     {{-- Breadcrumb Components --}}
                     <x-card>
                         <x-card-header title="Breadcrumb Components" />
