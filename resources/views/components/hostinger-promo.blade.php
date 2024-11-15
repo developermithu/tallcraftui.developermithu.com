ef5c1d2cfad2a33362be365921b5c3a3476f7886
developermithu <developermithu@gmail.com>
2024-11-16 04:17:33 +0600

    added hostinger referral link

---
+++ resources/views/components/hostinger-promo.blade.php
@@ -0,0 +1,26 @@
+<div
+    {{ $attributes->twMerge('mt-4 absolute w-full max-w-[26rem] rounded-lg border bg-gradient-to-r from-indigo-600 to-teal-600 text-white shadow-sm') }}>
+    <div class="p-4 px-8">
+        <div class="flex flex-col items-center space-y-4 text-center">
+            <h2 class="text-3xl font-bold text-white">Black Friday Mega Offer!</h2>
+            <p class="text-xl">Huge discounts on all Hostinger plans</p>
+            <p class="text-lg font-semibold">Use our referral link for an extra 20% off!</p>
+
+            <div class="pb-2">
+                <a data-pan="hostinger-referral-link" href="https://hostinger.com?REFERRALCODE=8Z0WEBDEVDOJ"
+                    target="_blank"
+                    class="inline-flex items-center justify-center h-10 gap-2 px-4 py-2 text-sm font-medium text-teal-600 transition-colors bg-white rounded-md ring-offset-background focus-visible:ring-ring whitespace-nowrap hover:bg-gray-100 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50">
+                    Claim Your Discount Now
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                        fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" class="size-4">
+                        <rect x="3" y="8" width="18" height="4" rx="1"></rect>
+                        <path d="M12 8v13"></path>
+                        <path d="M19 12v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7"></path>
+                        <path d="M7.5 8a2.5 2.5 0 0 1 0-5A4.8 8 0 0 1 12 8a4.8 8 0 0 1 4.5-5 2.5 2.5 0 0 1 0 5"></path>
+                    </svg>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
